c9a45c9f73462735e8e6db24e2179950261aaeb1
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-06-20 22:52:48 +0530

    added dom manipulation method

--- javascript/dom basics/main.js
+++ javascript/dom basics/main.js
@@ -20,3 +20,48 @@ const item = document.querySelectorAll('.item');
 items.forEach((item) => console.log(item));
 
 //manipulating DOM
+const ul = document.querySelector('ul');
+ul.remove();
+ul.lastElementChild.remove(); //removes last element in the ul array
+uli.children[1].textContent = 'Hi';
+uli.lastElementChild.innerHTML='<h4>Hello</h4>'; //dynamic changes to html content
+
+const btn = document.querySelector('.btn');
+btn.style.background='red'; //changes the background of button to red
+
+//events
+btn.addEventListener('click', (e) => {
+    e.preventDefault(); //to prevent default behaviour of button clicks
+    console.log(e.target);  //returns the submit div
+});
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault(); //to prevent default behaviour of button clicks
+    document.querySelector('#my-form').style.background = '#ccc'; //changes the background color of the form when click event happens
+});
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+    if (nameInput.value == '' || emailInput.value == ''){
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+        
+        setTimeout(()=> msg.remove(), 3000);    //removes message after 3 seconds
+    } else {
+    //if user enters all needed inputs    
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+    userList.appendChild(li);
+    //clear fields after adding to items list
+    nameInput.value= '';
+    emailInput.value= '';
+}
+};
