27aca73afcac05cabfbcb7ef825606c131001b9a
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-06-22 23:56:10 +0530

    added ngFor directive example

--- angular/my-first-app/src/app/servers/servers.component.ts
+++ angular/my-first-app/src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus='No server was created';
   serverName = 'Testserver';
   serverCreated = false;
+  servers = ['Testserver1','Testserver2']; 
 
   constructor() {
     setTimeout(() => {
@@ -24,6 +25,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server Created'
   }
 
