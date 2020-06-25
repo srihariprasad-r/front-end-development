ebd68e040423a1f064891020959b9899ef667bbd
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-06-25 22:38:18 +0530

    fixed image size

--- angular/shop-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular/shop-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,7 @@ export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Test Recipe','This is a test','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
-    new Recipe('Test Recipe','This is a test','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
+    new Recipe(' Another Test Recipe','This is a test','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
   ];
 
   constructor() { }
