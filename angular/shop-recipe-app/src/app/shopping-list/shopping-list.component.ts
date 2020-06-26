7981c4f1067211fae26d7fb2abdc844e53ca8694
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-06-26 23:54:33 +0530

    added local reference event binding for shopping list component

--- angular/shop-recipe-app/src/app/shopping-list/shopping-list.component.ts
+++ angular/shop-recipe-app/src/app/shopping-list/shopping-list.component.ts
@@ -17,4 +17,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onIngredientAdded(newingredient: Ingredient) {
+    this.ingredients.push(newingredient);
+  }
+
 }
