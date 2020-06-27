732aff973da836fdd432e7832bbf9c44c6d3d560
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-06-28 01:28:15 +0530

    added shared model and expanded recipe to ingredients

--- angular/shop-recipe-app/src/app/recipes/recipe.service.ts
+++ angular/shop-recipe-app/src/app/recipes/recipe.service.ts
@@ -1,10 +1,25 @@
 import { Recipe } from './recipes.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     private recipes: Recipe[] = [
-        new Recipe('Test Recipe','This is a test','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
-        new Recipe(' Another Test Recipe','This is a test','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
+        new Recipe('Schezwan Fried Rice',
+        'Its so good!',
+        'https://i2.wp.com/www.vegrecipesofindia.com/wp-content/uploads/2014/06/schezwan-fried-rice-recipe-500x375.jpg', 
+        [
+            new Ingredient('Rice', 1),
+            new Ingredient('French Fries', 20)
+        ]
+        ),
+
+        new Recipe('Veg Burger Recipe ',
+        'What else you need to say!',
+        'https://www.indianhealthyrecipes.com/wp-content/uploads/2016/02/veg-burger-recipe-1-500x375.jpg',
+        [
+            new Ingredient('Bread', 2),
+            new Ingredient('French Fries', 20)
+        ])
       ];    
 
     recipeSelected = new EventEmitter<Recipe>();
