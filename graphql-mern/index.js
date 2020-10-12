ac1cda6a6cc4e5b2838e3474593034dc4d15364d
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-10-13 00:07:27 +0530

    added graphql wrkdir with composite queries

--- graphql-mern/index.js
+++ graphql-mern/index.js
@@ -1,21 +1,36 @@
 const { graphql, buildSchema } = require('graphql');
 
+const db = {
+    usr: [
+        {name: 'A', email: 'random2@email.com'},
+        {name: 'V', email: 'random1@email.com'}
+    ]
+}
+
 const schema = buildSchema(
     `type Query {
-        message: String
-    }`
+        users: [User!]!
+    }
+    
+    type User {
+        name: String!,
+        email: String
+    }
+    `
 )
 
 const value = {
-    message:() => 'This is a message for GraphQL testing'
+    users :() => db.usr
 }
 
 graphql(
     schema,
     `
     {
-        message
+        users {
+            email
+        }
     }`,
     value
-).then (console.log) 
+).then (res => console.dir(res, {depth: null})) 
 .catch (console.error)
