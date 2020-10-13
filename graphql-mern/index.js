e2f65a2917bd41d9ddec3bf8bc7f96a6dc8eb415
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-10-14 00:54:35 +0530

    added example for nested queries

--- graphql-mern/index.js
+++ graphql-mern/index.js
@@ -6,6 +6,11 @@ const db = {
     users: [
         {name: 'A', email: 'random2@email.com'},
         {name: 'V', email: 'random1@email.com'}
+    ],
+    messages: [
+        {id: '1', userid:"A",  body: 'Hello'},
+        {id: '2', userid:"V",  body: 'Hey'},
+        {id: '3', userid:"A",  body: 'Hi'},        
     ]
 }
 
@@ -13,6 +18,7 @@ const schema = buildSchema(
     `type Query {
         users: [User]
         user(name:String!): User
+        messages: [Message!]!
     }
 
     type Mutation {
@@ -21,13 +27,30 @@ const schema = buildSchema(
     
     type User {
         name: String!,
-        email: String
+        email: String,
+        messages: [Message!]!
     }
+
+    type Message {
+        id: String!,
+        body: String
+    }    
     `
 )
 
+class User {
+    constructor(user){
+        Object.assign(this, user)
+    }
+    messages() {
+        return db.messages.filter(message => message.userid === this.name)
+    }
+}
+
+
 const value = {
-    users: () => db.users,
+    users: () => db.users.map(user => new User(user)),
+    messages : () => db.messages,
     user: args => db.users.find(user => args.name === user.name),
     adduser: ({name, email}) => {
         const user = {
