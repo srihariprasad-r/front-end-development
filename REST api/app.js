8a5bcccf9f6944af0860c0a8d9895394fa12b554
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-10-15 01:10:24 +0530

    added error handler

--- REST api/app.js
+++ REST api/app.js
@@ -7,4 +7,21 @@ const orderResources = require('./api/routes/orders')
 app.use('/products', productResources)
 app.use('/orders', orderResources)
 
+app.use((req, res, next) => {
+    const error = new Error('Resource is not found!');
+    error.status = 404
+    next(error)
+});
+
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
+
+
 module.exports = app;
