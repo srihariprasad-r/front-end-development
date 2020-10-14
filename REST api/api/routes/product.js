77aadc7ef1482ab5e817a8569bdd74bc92857a98
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-10-15 00:11:08 +0530

    added params in get request

--- REST api/api/routes/product.js
+++ REST api/api/routes/product.js
@@ -13,4 +13,20 @@ router.post('/', (req, res, next) => {
     })
 });
 
+
+router.get('/:productID', (req, res, next) => {
+    const productID = req.params.productID;
+    if (productID === "123") {
+    res.status(200).json({
+        message:"GET method with /product ID",
+        ID: productID
+    })
+    } else {
+        res.status(200).json({
+            message:"GET method with /product ID was not found!",
+            ID: productID
+        })
+    }
+});
+
 module.exports = router;
