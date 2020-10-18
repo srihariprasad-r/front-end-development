45a19ad1a89e085965c0722cdb5ee6aeba1e35b8
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-10-19 00:44:57 +0530

    added comments service listener

---
+++ microservices/blog/comments/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyparser.json());
+
+const commentsbyID = {};
+
+app.get('/posts/:id/comments', (req,res) => {
+    res.send(commentsbyID[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req,res) => {
+    const id = randomBytes(4).toString('hex');
+    const { content } = req.body;
+
+    const comments = commentsbyID[req.params.id] || [];
+
+    comments.push({id: id, content});
+
+    commentsbyID[req.params.id] = comments;
+
+    res.status(201).send(commentsbyID[req.params.id]);
+});
+
+app.listen(4001, () => {
+    console.log('Listening at port 4001')
+});
