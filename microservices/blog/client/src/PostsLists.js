50a4099281c1280f09cde98f827ab04155ed0fe1
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-10-20 00:56:10 +0530

    added commentcreate

--- microservices/blog/client/src/PostsLists.js
+++ microservices/blog/client/src/PostsLists.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import CommentsCreate from './CommentsCreate';
 
 export default () => {
     const [posts, setPosts] = useState({});
@@ -19,6 +20,7 @@ export default () => {
         key={post.id}>
             <div className="card-body">
                 <h3>{post.title}</h3>
+                <CommentsCreate postId={post.id}/>
             </div>
         </div>
         );
