af9e8ca05e79e82fc6ddc69657cf0298bcab46dd
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-10-20 00:35:29 +0530

    added postcreate and postlists

--- microservices/blog/posts/index.js
+++ microservices/blog/posts/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyparser.json());
+app.use(cors());
 
 const posts = {};
 
