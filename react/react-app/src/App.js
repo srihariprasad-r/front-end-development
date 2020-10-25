c5a368ac69f15a6506560db724f3f97920c14b6a
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-10-25 19:26:01 +0530

    added state and setstate functionalities

--- react/react-app/src/App.js
+++ react/react-app/src/App.js
@@ -3,12 +3,30 @@ import './App.css';
 import Person from './Person/Person';
 
 class App  extends  Component{
+  state = {
+    persons : [
+      {name: "ABC", age: 45},
+      {name: "DEC", age: 88}
+    ]
+  };
+
+  switchNameHandler = () => {
+    // this.state.persons.persons[0].name = "ABD"  // ERROR!
+    this.setState({
+      persons : [
+        {name: "ABD", age: 45},
+        {name: "DEC", age: 48}
+      ]
+    })
+  };
+
   render () {
   return (
     <div className="App">
       <h1>I am a React App</h1>
-      <Person name="ABC" age="45"/>
-      <Person name="DEC" age="88"> My hobbies: Racing </Person>
+      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies: Racing </Person>
     </div>
   );
   }
