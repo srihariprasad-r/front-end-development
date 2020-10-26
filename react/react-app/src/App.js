accc5d70db01c70bed2c26b5c998a9f2e0bb755c
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-10-26 19:58:40 +0530

    added toggling feature for dynamic display

--- react/react-app/src/App.js
+++ react/react-app/src/App.js
@@ -7,7 +7,8 @@ class App  extends  Component{
     persons : [
       {name: "ABC", age: 45},
       {name: "DEC", age: 88}
-    ]
+    ],
+    showPersons: false
   };
 
   switchNameHandler = (newName) => {
@@ -31,23 +32,35 @@ class App  extends  Component{
     })
   };
 
+  toggleHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    })
+  }
+
   render () {
   return (
     <div className="App">
       <h1>I am a React App</h1>
-      <button onClick={this.switchNameHandler}>Switch Name</button>
-      <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age} 
-        namechange={this.changeNameHandler}
-      />
-      <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        click={this.switchNameHandler.bind(this,'ABCD')}
-      > 
-      My hobbies: Racing 
-      </Person>
+      <button onClick={this.toggleHandler}>Toggle Name</button>
+      { this.state.showPersons ?
+          <div>
+              <Person 
+              name={this.state.persons[0].name} 
+              age={this.state.persons[0].age} 
+              namechange={this.changeNameHandler}
+              />
+              <Person 
+              name={this.state.persons[1].name} 
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this,'ABCD')}
+              > 
+              My hobbies: Racing 
+              </Person>    
+          </div> 
+        : null
+    }    
     </div>
   );
   }
