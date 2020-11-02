e0bba7bdbe918092dc86afbc721bb1862e790d12
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-11-03 00:26:29 +0530

    added data object for dynamic parsing of values

--- react/practise/react-dev/frontend/src/App.js
+++ react/practise/react-dev/frontend/src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import React from 'react';
+import data from './data';
 import './App.css';
 
 function App() {
@@ -37,72 +38,21 @@ function App() {
       <main className="main">
         <div className="content">
           <ul className="products">
-            <li>
-              <div className="product">
-                <img src="./images/d1.jpg" alt="products" className="product-image" />
-                <div className="product-name">
-                    <a href="product.html>" Slim Shirt>Slim Shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars(10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img src="./images/d1.jpg" alt="products" className="product-image" />
-                  <div className="product-name">
-                    <a href="product.html>" Slim Shirt>Slim Shirt</a>
+            {
+              data.products.map(product => 
+                <li>
+                  <div className="product">
+                    <img src={product.image} alt="products" className="product-image" />
+                    <div className="product-name">
+                      <a href="product.html>" Slim Shirt>{product.name}</a>
+                    </div>
+                    <div className="product-brand">{product.brand}</div>
+                    <div className="product-price">{product.price}</div>
+                    <div className="product-rating">{product.rating} Stars({product.numreviews} Reviews)</div>
                   </div>
-                  <div className="product-brand">Nike</div>
-                  <div className="product-price">$60</div>
-                  <div className="product-rating">4.5 Stars(10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img src="./images/d1.jpg" alt="products" className="product-image" />
-                <div className="product-name">
-                  <a href="product.html>" Slim Shirt>Slim Shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars(10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img src="./images/d1.jpg" alt="products" className="product-image" />
-                <div className="product-name">
-                  <a href="product.html>" Slim Shirt>Slim Shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars(10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img src="./images/d1.jpg" alt="products" className="product-image" />
-                <div className="product-name">
-                  <a href="product.html>" Slim Shirt>Slim Shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars(10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img src="./images/d1.jpg" alt="products" className="product-image" />
-                <div className="product-name">
-                  <a href="product.html>" Slim Shirt>Slim Shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars(10 Reviews)</div>
-              </div>
-            </li>                                    
+                </li>  
+              )                                    
+            }
           </ul>
         </div>
       </main>
---
+++ react/practise/react-dev/frontend/src/data.js
@@ -0,0 +1,42 @@
+export default {
+    products: [
+    {
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: './images/d1.jpg',
+        price: '$60',
+        brand: 'Nike',
+        rating: 4.5,
+        numreviews: 40
+    },
+    {
+            name: 'Fit Shirt',
+            category: 'Shirts',            
+            image: './images/d1.jpg',
+            price: '$50',
+            brand: 'Nike',
+            rating: 4.3,
+            numreviews: 12
+    },
+    {
+            name: 'Pant',
+            category: 'Pants',            
+            image: './images/d1.jpg',
+            price: '$90',
+            brand: 'Nike',
+            rating: 4.7,
+            numreviews: 14
+    },        
+    {
+            name: 'Pant Showroom',
+            category: 'Pants',
+            image: './images/d1.jpg',
+            price: '$45',
+            brand: 'Nike',
+            rating: 3.9,
+            numreviews: 1
+     }     
+
+
+]
+}
