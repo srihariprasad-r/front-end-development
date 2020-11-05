9ac73fb6a4a96017f8acf31a8aa293722ccc3b89
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-11-05 19:25:55 +0530

    added redux module with store

---
+++ react/practise/react-dev/frontend/src/reducers/productReducers.js
@@ -0,0 +1,18 @@
+export const PRODUCT_LIST_REQUEST = "PRODUCT_LIST_REQUEST";
+export const PRODUCT_LIST_SUCCESS = "PRODUCT_LIST_SUCCESS";
+export const PRODUCT_LIST_FAIL = "PRODUCT_LIST_FAIL";
+
+function productListReducer(state = { products: [] }, action) {
+    switch(action.type) {
+        case PRODUCT_LIST_REQUEST:
+            return {loading: true};
+        case PRODUCT_LIST_SUCCESS:
+            return {loading: false, products: action.payload};
+        case PRODUCT_LIST_FAIL:
+            return {loading: false, error: action.payload};
+        default:
+            return state;
+    }       
+};
+
+export { productListReducer };
