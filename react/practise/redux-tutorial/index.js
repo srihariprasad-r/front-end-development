00cd3c9f8ea9167c0444d05fc0ee4558f57a50ca
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-11-08 00:08:08 +0530

    added redux properties for reducer and store

--- react/practise/redux-tutorial/index.js
+++ react/practise/redux-tutorial/index.js
@@ -1,3 +1,6 @@
+const redux = require('redux');
+const createstore = redux.createStore;
+
 /** Action */
 const BUY_CAKE = 'BUY_CAKE';
 
@@ -7,4 +10,33 @@ function buyCake() {
         info: 'First action'        
     }
 }
+
+/** Reducer 
+ * (prevState, action) => newState
+*/
+const initialState = {
+    numOfCakes : 10
+}
+
+const reducer = (state= initialState, action) => {
+    switch(action.type) {
+        case 'BUY_CAKE':
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - 1
+            }
+        default:
+            return state
+    }
+}
+
+/** Store */
+
+const store = createstore(reducer);
+console.log('Initial State', store.getState());     //fetch currentState
+const unsubscribe = store.subscribe(() => console.log('State change', store.getState()));  //subscribers to get notified upon change
+store.dispatch(buyCake()); //dispatch an action
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
 
