b49d69ee798e5c38d27fc53258d046a51c997c12
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-11-08 00:18:47 +0530

    added multiple action dispatches and combinereducer

--- react/practise/redux-tutorial/index.js
+++ react/practise/redux-tutorial/index.js
@@ -1,8 +1,10 @@
 const redux = require('redux');
 const createstore = redux.createStore;
+const combinereducer = redux.combineReducers;
 
 /** Action */
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = "BUY_ICECREAM";
 
 function buyCake() {
     return {
@@ -11,32 +13,68 @@ function buyCake() {
     }
 }
 
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+    info: "First action",
+  };
+}
+
 /** Reducer 
  * (prevState, action) => newState
 */
-const initialState = {
-    numOfCakes : 10
-}
 
-const reducer = (state= initialState, action) => {
-    switch(action.type) {
-        case 'BUY_CAKE':
-            return {
-                ...state,
-                numOfCakes: state.numOfCakes - 1
-            }
-        default:
-            return state
+/**
+const initialState = {
+    numOfCakes : 10,
+    numOfIcecream : 20
+}
+*/
+
+const initialCake = {
+  numOfCakes: 10
+};
+
+const initialIcecream = {
+  numOfIcecream: 20,
+};
+
+
+const Cakereducer = (state= initialCake, action) => {
+    switch (action.type) {
+      case "BUY_CAKE":
+        return {
+          ...state,
+          numOfCakes: state.numOfCakes - 1,
+        };
+      default:
+        return state;
     }
 }
 
-/** Store */
+const Icecreamreducer = (state = initialIcecream, action) => {
+  switch (action.type) {
+    case "BUY_ICECREAM":
+      return {
+        ...state,
+        numOfIcecream: state.numOfIcecream - 1,
+      };
+    default:
+      return state;
+  }
+};
 
+/** Store */
+const reducer = combinereducer({
+    Cake: Cakereducer,
+    Icecream: Icecreamreducer
+});
 const store = createstore(reducer);
 console.log('Initial State', store.getState());     //fetch currentState
 const unsubscribe = store.subscribe(() => console.log('State change', store.getState()));  //subscribers to get notified upon change
 store.dispatch(buyCake()); //dispatch an action
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());      //new dispatch action created for Icecream
 unsubscribe();
 
