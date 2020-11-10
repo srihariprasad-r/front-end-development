2350671ec2a546996d7bf1ebec77afebdb041090
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-11-10 09:48:06 +0530

    initial version

---
+++ react/practise/components/src/index.js
@@ -0,0 +1,30 @@
+import React from  'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" />                
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Mr.Author
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 06:00 PM</span>                        
+                    </div>
+                    <div className="text">Blog post</div>
+                </div>
+            </div>
+        </div>
+
+
+    );
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
