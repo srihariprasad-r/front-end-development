15d5669b5b018daec439a77777250bf8139346bb
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-11-11 10:00:15 +0530

    added props with components passed into other components and some ui changes

--- react/practise/components/src/index.js
+++ react/practise/components/src/index.js
@@ -1,20 +1,25 @@
 import React from  'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
       <div className="ui container comments">
-        <CommentDetail 
-            authorName="Sam" 
-            timeAgo="Today at 03:05PM" 
+        <ApprovalCard>
+          <CommentDetail
+            authorName="Sam"
+            timeAgo="Today at 03:05PM"
             comment="How are you?"
-        />
-        <CommentDetail 
-            authorName="Alex" 
-            timeAgo="Today at 13:25PM" 
-            comment="Happy Monday!" 
-        />
+          />
+        </ApprovalCard>
+        <ApprovalCard>
+            <CommentDetail
+              authorName="Alex"
+              timeAgo="Today at 13:25PM"
+              comment="Happy Monday!"
+            />
+        </ApprovalCard>
       </div>
     );
 };
