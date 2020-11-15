1922eb723cee74022c0d1eabac747e1e521a5ade
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-11-15 22:43:27 +0530

    added lifecycle components

---
+++ react/practise/lifecycle-components/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
+class App extends React.Component {
+    state = {lat: null, errorMessage: ''};
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat: position.coords.latitude}),
+            error => this.setState({ errorMessage: error.message})
+        );
+    }
+
+
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        } 
+        
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+        return <div>Loading..</div> 
+    };
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
