a2719a4554ff3c98f1cbb2a2839cdd8fb58fabb1
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-11-26 17:31:58 +0530

    added initial graphql playground on apollo

--- graphql-mern/index.js
+++ graphql-mern/index.js
@@ -1,89 +1,26 @@
-const express  = require('express')
-const { graphqlHTTP } = require('express-graphql')
-const { graphql, buildSchema } = require('graphql')
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
 
-const db = {
-    users: [
-        {name: 'A', email: 'random2@email.com'},
-        {name: 'V', email: 'random1@email.com'}
-    ],
-    messages: [
-        {id: '1', userid:"A",  body: 'Hello'},
-        {id: '2', userid:"V",  body: 'Hey'},
-        {id: '3', userid:"A",  body: 'Hi'},        
-    ]
-}
-
-const schema = buildSchema(
-    `type Query {
-        users: [User]
-        user(name:String!): User
-        messages: [Message!]!
+const typeDefs = gql `
+    type Query {
+        sayHi: String!
     }
+`;
 
-    type Mutation {
-        adduser(name: String!, email: String): User
-    }
-    
-    type User {
-        name: String!,
-        email: String,
-        messages: [Message!]!
-    }
-
-    type Message {
-        id: String!,
-        body: String
-    }    
-    `
-)
-
-class User {
-    constructor(user){
-        Object.assign(this, user)
-    }
-    messages() {
-        return db.messages.filter(message => message.userid === this.name)
-    }
-}
-
-
-const value = {
-    users: () => db.users.map(user => new User(user)),
-    messages : () => db.messages,
-    user: args => db.users.find(user => args.name === user.name),
-    adduser: ({name, email}) => {
-        const user = {
-            name : name,
-            email: email
-        } 
-        db.users.push(user)
-        return user
-    }
-}
-
-const app = express()
-
-app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: value,
-    graphiql: true,
-  }))
-  
-
-app.listen(2000, () => console.log('Listening at the port 2000'))
-
-/** 
-graphql(
-    schema,
-    `
-    {
-        users {
-            email
+const resolvers = {
+    Query : {
+        sayHi() {
+            return 'Hello world!'
         }
-    }`,
-    value
-).then (res => console.dir(res, {depth: null})) 
-.catch (console.error)
+    }
+};
 
-*/
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen(
+    {port: 5000}
+    ).then(res => console.log(`Server running at ${res.url}`))
+    .catch(err=> Error(err));
