50ca40cc2683656de6aad785b0639dc6ed6cbf49
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-11-26 23:03:56 +0530

    added mutation

---
+++ graphql-mern/graphql/resolvers/users.js
@@ -0,0 +1,12 @@
+const User = require("../../models/User");
+
+module.exports = {
+    Mutation: {
+        register(_ , args, context, info) {
+            //TODO: Validate data
+            //TODO: User doesn't exist
+            //TODO: Hash password and create token
+            
+        }
+    }
+}
--- graphql-mern/index.js
+++ graphql-mern/index.js
@@ -2,33 +2,10 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
-const { MONGODB } = require('./nodemon.js');
-const Post = require('./models/Post');
+const { MONGODB } = require('./config.js');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 
-const typeDefs = gql`
-  type Post {
-    id: ID!,
-    body: String!,
-    username: String!,
-    createdAt: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`;
-
-const resolvers = {
-    Query : {
-        async getPosts() {
-            try {
-                const posts = await Post.find();
-                return posts;
-            }catch(err) {
-                throw new Error(err);
-            }
-        }
-    }
-};
 
 const server = new ApolloServer({
     typeDefs,
