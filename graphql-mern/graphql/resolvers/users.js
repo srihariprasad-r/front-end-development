69be30fcd12bb3a00dfe81daa4f1f233e476837c
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-11-27 00:08:00 +0530

    added jwt and hashing algorthms

--- graphql-mern/graphql/resolvers/users.js
+++ graphql-mern/graphql/resolvers/users.js
@@ -1,12 +1,52 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require("../../models/User");
+const { UserInputError } = require('apollo-server');
+
+const { SECRET_KEY } = require('../../config');
 
 module.exports = {
     Mutation: {
-        register(_ , args, context, info) {
+        async register(
+        _ , 
+        {
+            registerInput: { username, email, password, confirmpassword}
+        }
+        ) {
             //TODO: Validate data
             //TODO: User doesn't exist
-            //TODO: Hash password and create token
-            
+            const userexists = User.findOne({username});
+
+            if (userexists) {
+                throw new UserInputError('User name exists', {
+                    errors: {
+                        username: 'This username exists'
+                    }
+                })
+            }
+            //Hash password and create token
+            password = await bcrypt.hash(password, 12);
+
+            const newUser = new User({
+                email,
+                username,
+                password,
+                createdAt: new Date().toISOString()
+            });
+
+            const res = await newUser.save();
+
+            const token = jwt.sign({
+                id: res.id,
+                email: res.email,
+                username: res.username
+            }, SECRET_KEY, {expiresIn: '1h'});
+
+            return {
+                ...res._doc,
+                id: res._id,
+                token
+            }
         }
     }
 }
