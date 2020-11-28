26c8ea1b5aac5dff74a57d1208c52a000d19d06a
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-11-29 01:14:21 +0530

    added nested type relations

--- graphql/basics/schema/schema.js
+++ graphql/basics/schema/schema.js
@@ -3,8 +3,14 @@ const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLInt } = graphql;
 
 var books = [
-    {name: 'Book1', id: '1', genre:'Sci-fi'},
-    {name: 'Book2', id: '2', genre:'Comedy'}
+    {name: 'Book1', id: '1', genre:'Sci-fi', authorid:'1'},
+    {name: 'Book2', id: '2', genre:'Comedy', authorid:'2'}
+]
+
+
+var books = [
+    {name: 'Author1', id: '1', age:42},
+    {name: 'Author2', id: '2', ager:66}
 ]
 
 const BookType = new GraphQLObjectType({
@@ -12,7 +18,13 @@ const BookType = new GraphQLObjectType({
     fields: () => ({
         id: {type: GraphQLString},
         name: {type: GraphQLString},
-        genre: {type: GraphQLString}
+        genre: {type: GraphQLString},
+        author: {
+            type: AuthorType,
+            resolve(parents, args) {
+                return authors.filter(item => item.id === parents.authorid)
+            }
+        }
     })
 });
 
@@ -37,6 +49,15 @@ const RootQueryType = new GraphQLObjectType({
             resolve(parent, args) {
                 return books.filter(item => item.id === args.id)
             }
+        },
+        author: {
+            type: AuthorType,
+            args: {
+                id:{type: GraphQLString}
+            },
+            resolve(parent, args) {
+                return authors.filter(item => item.id === args.id)
+            }            
         }
     }
 });
