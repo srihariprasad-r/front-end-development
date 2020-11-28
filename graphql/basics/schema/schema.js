b43a704df7a4117f29586fca8061364d7dbc0d20
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-11-29 00:42:41 +0530

    added schemas and object types

--- graphql/basics/schema/schema.js
+++ graphql/basics/schema/schema.js
@@ -1,6 +1,11 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLInt } = graphql;
+
+var books = [
+    {name: 'Book1', id: '1', genre:'Sci-fi'},
+    {name: 'Book2', id: '2', genre:'Comedy'}
+]
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -8,6 +13,16 @@ const BookType = new GraphQLObjectType({
         id: {type: GraphQLString},
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
+    })
+});
+
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        age: {type: GraphQLInt}
     })
 });
 
@@ -20,7 +35,7 @@ const RootQueryType = new GraphQLObjectType({
                 id:{type: GraphQLString}
             },
             resolve(parent, args) {
-
+                return books.filter(item => item.id === args.id)
             }
         }
     }
--- graphql/basics/server/app.js
+++ graphql/basics/server/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 
-const schema = require('../schema/schema');
+const graphschema = require('../schema/schema');
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-    schema: schema,
+    schema: graphschema,
     graphiql: true
 }));
 
