3390e57e09e096a67559e15c45af43a650315fc8
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-12-03 00:31:26 +0530

    added modifiers to reduce db call

--- graphql/merng/graphql/resolvers/index.js
+++ graphql/merng/graphql/resolvers/index.js
@@ -3,11 +3,16 @@ const userResolvers = require("./users");
 const commentsResolvers = require('./comments');
 
 module.exports = {
+    Post: {
+        likeCount: (parent) => parent.likes.length,
+        commentCount : (parent) => parent.comments.length,
+    },
     Query: {
         ...postResolvers.Query
     },
     Mutation: {
         ...userResolvers.Mutation,
-        ...postResolvers.Mutation
+        ...postResolvers.Mutation,
+        ...commentsResolvers.Mutation
     }
 }
