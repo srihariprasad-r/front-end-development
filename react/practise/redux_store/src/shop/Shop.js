60ccd7ff4af40c210b34e842473cc8f0819a239c
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-12-04 19:37:47 +0530

    added hooks

---
+++ react/practise/redux_store/src/shop/Shop.js
@@ -0,0 +1,72 @@
+import React, { useState , useEffect } from 'react';
+
+const Shop = () => {
+
+    const [cart, setCart] = useState([]);
+    const [alert, setAlert] = useState("");
+
+    const items = [
+        {
+            id: 1,
+            name: 'Xbox',
+            price: 300
+        },
+        {
+            id: 2,
+            name: 'Playstation',
+            price: 600
+        },
+        {
+            id: 3,
+            name: 'Brand X',
+            price: 255
+        }    
+    ];
+
+    const addToCart = (el) => {
+        let flag = true;
+        for (let i =0; i < cart.length; i++){
+            if (cart.id === el.id) {
+                flag = false;
+            }
+        } 
+        if (flag) {
+            setCart([...cart, el]);
+        } else {
+            setAlert(`${el.name} is already in the cart!`);
+        }
+    };
+
+    const removeFromCart = (el) => {
+        let hardcopy = [...cart];
+        hardcopy = hardcopy.filter(cartitem => cartitem.id !== el.id);
+        setCart(hardcopy);
+    };
+    
+    
+    const listItems = items.map((el) => (
+        <div key={el.id}>
+            {`${el.name}: $${el.price}`}
+            <span> <input type="submit" value="Add" onClick={() => addToCart(el)} /> </span>
+        </div>
+    ));
+
+    const cartItems = cart.map((el) => (
+        <div key={el.id}>
+            {`${el.name}: $${el.price}`}
+            <span> <input type="submit" value="Remove" on-Click={() => removeFromCart(el)} /> </span>
+        </div>
+    ));
+
+    return (
+    <div>
+        STORE
+        <div>{listItems}</div>
+        <div>Cart Items</div>
+        <div>{cartItems}</div>
+        <div>Alert message: {alert}</div>
+    </div>
+    );
+}
+
+export default Shop;
