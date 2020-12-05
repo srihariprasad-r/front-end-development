753e520324f55f079caf1146d1b130689910e6cf
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-12-06 00:35:00 +0530

    added practise code for react hooks- usereducer hook examples

--- react/practise/practise/src/reducerhook.js
+++ react/practise/practise/src/reducerhook.js
@@ -1,4 +1,4 @@
-import {useReducer} from 'react';
+import {useReducer, useState} from 'react';
 
 export const ReducerHook = () => {
     function reducerfunction(state, action) {
@@ -6,19 +6,42 @@ export const ReducerHook = () => {
             case 'INCREMENT':
                 return state + 1;
             case 'DECREMENT':
-                return state - 1;   
+                return state - 1;  
+            case 'ADD_TODO':
+                return {todos: [...state.todos, {text: action.text, completed: 'false'}], todoCount: state.todoCount + 1} 
+            case 'UPDATE_COMPLETED':
+                return {
+                    ...state,
+                    todos: state.todos.map((e, idx)=> idx === action.index ? {...e, completed: !e.completed}: e),
+                }
             default:
                 return state;          
         }
     };
 
     const [counter, dispatch] = useReducer(reducerfunction, 0);
+    const [{todos, todoCount}, setTodo] = useReducer(reducerfunction, {todos:[], todoCount:0});
+    const [text, setText] = useState("");
 
     return (
         <div>
                 <button onClick={()=> dispatch({type:'INCREMENT'})}>Reducer+</button>
                 <button onClick={()=> dispatch({type:'DECREMENT'})}>Reducer-</button>
                 <div>reducercount: {counter}</div>
+                <form onSubmit={e => {
+                    e.preventDefault();
+                    setTodo({type: 'ADD_TODO', text});
+                    setText("");
+                    }}>
+                    <input value={text} onChange={e => setText(e.target.value)} />
+                </form>
+                {todos.map((e, index)=> (
+                    <div key={e.text} onClick={() => setTodo({type:'UPDATE_COMPLETED', index})}>
+                        {e.text}
+                        {e.completed}
+                    </div>
+                ))}
+                <div>Todo Count: {todoCount}</div>
         </div>
     );
 
