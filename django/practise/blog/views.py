2a4bef9ec97ddce1fae3ec172059225f04974487
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-12-12 19:15:34 +0530

    added templates

--- django/practise/blog/views.py
+++ django/practise/blog/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog returned</h1>')
+    # return HttpResponse('<h1>Blog returned</h1>')
+    return render(request, 'blog/home.html')
 
 
 def about(request):
-    return HttpResponse('<h1>You are on About page!</h1>')    
+    #return HttpResponse('<h1>You are on About page!</h1>')    
+    return render(request, 'blog/about.html')
