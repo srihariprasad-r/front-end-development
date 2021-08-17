13f669ad9bfd9ff28e06a27b9ec0375ae66f9d60
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-08-17 22:04:09 +0530

    added db and marshallow imports

--- REST api/flask/app.py
+++ REST api/flask/app.py
@@ -4,6 +4,13 @@ from flask_marshmallow import Marshmallow
 import os
 
 app= Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'db.sqlite')
+app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 @app.route('/', methods=['GET'])
 def get():
