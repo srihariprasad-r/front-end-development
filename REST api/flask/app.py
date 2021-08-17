3e6e2268a9b8fce372c7fa9ce11d04e4092fd61b
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-08-17 23:20:15 +0530

    added db routes

--- REST api/flask/app.py
+++ REST api/flask/app.py
@@ -12,9 +12,50 @@ app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-@app.route('/', methods=['GET'])
-def get():
-    return jsonify({'msg':'Hello world!'})
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120))
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'price', 'quantity')
+
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
+
+@app.route('/product',methods=['POST'])
+def add_product():
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    quantity = request.json['quantity']
+
+    new_product = Product(name, description, price, quantity)
+    db.session.add(new_product)
+    db.session.commit()
+
+    return product_schema.jsonify(new_product)
+
+@app.route('/products',methods=['GET'])
+def get_all_products():
+    all_products = Product.query.all()
+    result = products_schema.dump(all_products)
+    return jsonify(result)
+
+@app.route('/product/',methods=['GET'])
+def get_product_by_id_quantity():
+    args = request.args
+    product = Product.query.filter(Product.quantity == args['quantity']).all()
+    return product_schema.jsonify(product)
 
 if __name__ == '__main__':
     app.run(debug=True)
